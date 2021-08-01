ad182dff0ed61a5c40759c27c3bd67e56c401093
HajarSalah <Hajarsalah221@gmail.com>
2021-08-01 17:54:55 +0300

    Built the stuff in Cicle Class

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Circle {
+
+    double radius=10;
+
+    Circle (){};
+
+    Circle (double radius){
+       this.radius=radius;
+   }
+   double getArea(){
+        return Math.pow(this.radius,2.0)*(Math.PI);
+   }
 }
