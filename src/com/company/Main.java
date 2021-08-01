3cf183be2382ef2bda556059577e8c34f532a979
HajarSalah <Hajarsalah221@gmail.com>
2021-08-01 18:00:52 +0300

    Added the stuff in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Circle circle1 =new Circle ();
+	circle1.radius=1.0;
+	Circle circle2=new Circle ();
+	Circle circle3= new Circle(125);
+
+	System.out.println(circle1.radius+ " " + circle2.radius +" "+ circle3.radius);
+
     }
 }
