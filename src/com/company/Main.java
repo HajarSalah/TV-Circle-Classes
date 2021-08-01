4b51802536cc8dc031fc5ae54be7e6142eb738f9
HajarSalah <Hajarsalah221@gmail.com>
2021-08-01 18:40:39 +0300

    the stuff in Main related to TV were added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        TV tv= new TV();
+       // tv.setChannel(2);
+       // tv.setVolumeLevel(4);
+       // tv.turnOn();
+        System.out.println( tv.channel + "  " +  tv.volumeLevel + "  "+ tv.on );
     }
     }
