87d19754c4c3439c5da2c9aedf91b1ac5346c3b9
HajarSalah <Hajarsalah221@gmail.com>
2021-08-01 18:34:46 +0300

    Edited TV Class

--- src/com/company/TV.java
+++ src/com/company/TV.java
@@ -1,4 +1,36 @@
 package com.company;
 
 public class TV {
+
+    int channel=1;
+    int volumeLevel=1;
+    boolean on= false;
+
+    public TV (){}
+
+    public void turnOn (){
+        this.on= true;
+    }
+    public void turnOff (){
+        this.on=false;
+    }
+    public void setChannel (int newChannel) {
+        this.channel=newChannel;
+    }
+
+    public void setVolumeLevel(int volumeLevel) {
+        this.volumeLevel = volumeLevel;
+    }
+    public void channelUp() {
+        this.channel++;
+    }
+    public void channelDown() {
+        this.channel--;
+    }
+    public void volumeUp () {
+        this.volumeLevel++;
+    }
+    public void volumeDown () {
+        this.volumeLevel-- ;
+    }
 }
